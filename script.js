6d2ef9178c10906b4d54bc3d6e7b0bd73f25be4c
sampson2448 <teronsampson@gmail.com>
2024-05-27 15:39:26 -0400

    api function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("connection")
+// Unsplash API
+const count = 10;
+const apiKey = "API_Key";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}
+`;
+
+async function getPhotos(){
+    try{
+        const response = await fetch(apiUrl)
+        const data = await response.json()
+        console.log(data)
+    }catch(error){
+
+}}
+getPhotos();
